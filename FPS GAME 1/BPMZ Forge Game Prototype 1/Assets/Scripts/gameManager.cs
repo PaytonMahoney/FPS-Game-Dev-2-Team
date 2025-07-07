06c0d71b9babb049d4d585ce34c1fdab8939eb24
Mitch McCoy <mccoy.for.tennis@gmail.com>
2025-07-07 14:37:33 -0500

    gameManager
    Added Game Manager function and added mainBoss tag

--- FPS GAME 1/BPMZ Forge Game Prototype 1/Assets/Scripts/gameManager.cs
+++ FPS GAME 1/BPMZ Forge Game Prototype 1/Assets/Scripts/gameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class gameManager : MonoBehaviour
 {
@@ -10,16 +11,15 @@ public class gameManager : MonoBehaviour
     [SerializeField] GameObject menuWin;
     [SerializeField] GameObject menuLose;
 
-
+    public Image playerHPBar;
+    public GameObject playerDMGPanel;
     public bool isPaused;
     public GameObject player;
     public playerController playerScript;
+    public GameObject mainBoss;
 
-    float timescaleOrig;
-
-    //Boss Variable // Different AI
-    int bossDead;
-
+    float timescaleOriginal;
+    bool bossDead;
     public int enemyCount;
 
     void Awake()
@@ -27,13 +27,66 @@ public class gameManager : MonoBehaviour
         instance = this;
 
         player = GameObject.FindWithTag("Player");
+        mainBoss = GameObject.FindWithTag("MainBoss");
         playerScript = player.GetComponent<playerController>();
-        timescaleOrig = Time.timeScale;
+        timescaleOriginal = Time.timeScale;
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Pausing
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (menuActive == null)
+            {
+                statePause();
+                menuActive = menuPause;
+                menuActive.SetActive(true);
+            }
+            else if (menuActive == menuPause)
+            {
+                stateUnpause();
+            }
+        }
+
+        if (mainBoss.HP <= 0)
+        {
+            youWin();
+        }
+    }
+
+    public void statePause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void stateUnpause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = timescaleOriginal;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(false);
+        menuActive = null;
+    }
+
+    public void youLose()
+    {
+        statePause();
+        menuActive = menuLose;
+        menuActive.SetActive(true);
+    }
+
+    public void youWin()
+    {
+        statePause();
+        menuActive = menuWin;
+        menuActive.SetActive(true);
     }
 }
